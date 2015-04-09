1b80fe2ee5ca9fe0ff07825ed8f6293e22d9c5cd
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-04-09 00:17:00 -0400

    add application controller: current_user methods etc

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,23 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  # users
+
+  def new_user
+    @user ||= User.new
+  end
+
+  def current_user
+    @current_user ||= User.find_by_authentication_token(cookies[:authentication_token]) if cookies[:authentication_token]
+  end
+
+  helper_method :new_user, :current_user
+  
+  #authentication
+
+  def authenticate_user!
+    redirect_to signup_path, alert: "Not authorized" if current_user.nil?
+  end
+  
 end
