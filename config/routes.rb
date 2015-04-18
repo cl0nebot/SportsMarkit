cf1220cdf5a7b180382a36f44b59a707aa420a62
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-04-18 13:55:12 -0400

    populate sitemap page

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
     resources :leagues
     resources :fans
     resources :relationships
+    resources :sitemap
     
     get "login" => "sessions#new", as: :login
     get "signup" => "users#new", as: :signup
