73a13fb637af029a1f90cd667def64430c7ab697
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-04-18 12:47:12 -0400

    add facility controller, model, view

---
+++ app/models/facility.rb
@@ -0,0 +1,30 @@
+class Facility < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :use_for_slug, use: :slugged
+  acts_as_gmappable
+  
+  has_many :fans, as: :fannable
+  
+  def gmaps4rails_address
+  #describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki
+    "#{self.address_1}, #{self.city}, #{self.state}" 
+  end
+  
+  def use_for_slug
+    existing_facility = Facility.where('slug = ?', self.slug)
+    if existing_facility.present?
+      "#{name} #{self.city_and_state} #{existing_facility.count}"
+    else
+      "#{name} #{self.city_and_state}"
+    end
+  end
+  
+  def city_and_state
+    "#{city}-#{state}"
+  end
+  
+  def full_address
+    
+  end
+  
+end
