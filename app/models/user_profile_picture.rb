40c2e877fa69cd71cc85c571cd78060eff88e1f5
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-04-18 12:42:34 -0400

    set up user_profile_picture.rb model

--- app/models/user_profile_picture.rb
+++ app/models/user_profile_picture.rb
@@ -1,2 +1,15 @@
 class UserProfilePicture < ActiveRecord::Base
+  mount_uploader :photo, ImageUploader
+  belongs_to :user
+  validates :photo, :presence => true
+  
+  after_create :delete_existing_profile_picture
+  
+  def delete_existing_profile_picture
+    owner = self.user
+    if owner.user_profile_pictures.count > 1
+      owner.user_profile_pictures.first.delete
+    end
+  end
+  
 end
