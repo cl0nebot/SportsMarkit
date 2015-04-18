53541ab6c81dd84975cc7602565b2236178c2f3d
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-04-18 12:46:34 -0400

    add league controller, model, view

---
+++ app/models/league.rb
@@ -0,0 +1,15 @@
+class League < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :use_for_slug, use: :slugged
+  
+  has_many :fans, as: :fannable
+  
+  def use_for_slug
+    existing_league = League.where('slug = ?', self.slug)
+    if existing_league.present?
+      "#{name} #{state} #{existing_league.count}"
+    else
+      "#{name} #{state}"
+    end
+  end
+end
