0f7d138c5d898b1cdc4dabb5e6ebfe93a6954796
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-04-21 12:56:38 -0400

    add ajax to yes/no/maybe buttons for events

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -3,8 +3,19 @@ class AttendeesController < ApplicationController
   def create
     @attendee = Attendee.create(user_id: params[:attendee][:user_id], event_id: params[:attendee][:event_id], yes: params[:attendee][:yes], maybe: params[:attendee][:maybe], no: params[:attendee][:no])
     @attendee.save
+    @event = Event.find(@attendee.event_id)
     # @object = eval(@fan.fannable_type).find(@fan.fannable_id)
     # @class = @object.class
+    respond_to do |format|
+      format.js
+      format.html
+    end
+  end
+  
+  def update
+    @attendee = Attendee.find(params[:id])
+    @attendee.update_attributes(user_id: params[:attendee][:user_id], event_id: params[:attendee][:event_id], yes: params[:attendee][:yes], maybe: params[:attendee][:maybe], no: params[:attendee][:no])
+    @event = Event.find(@attendee.event_id)
     respond_to do |format|
       format.js
       format.html
