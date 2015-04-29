9dc36a7d52eb0f7298efbdc432454ce18ca87776
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-04-29 14:48:18 -0400

    Add sport photo association to sport.rb

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -1,2 +1,6 @@
 class Sport < ActiveRecord::Base
+  
+  
+  has_many :sport_photos #TODO dependent destroy?
+  accepts_nested_attributes_for :sport_photos, :reject_if => :all_blank, :allow_destroy => true
 end
