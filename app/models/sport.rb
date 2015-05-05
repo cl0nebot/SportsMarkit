b2279799af01db7e09471949cc58281a1e8807f0
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-05-05 11:38:00 -0400

    add default_photo method to sport.rb

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -3,4 +3,8 @@ class Sport < ActiveRecord::Base
   
   has_many :sport_photos #TODO dependent destroy?
   accepts_nested_attributes_for :sport_photos, :reject_if => :all_blank, :allow_destroy => true
+  
+  def default_photo
+    sport_photos.last.photo
+  end
 end
