740ec0ac86a18a2b1078cd3ddcf06f64f6dc8d3e
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-05-05 22:16:40 -0400

    rm more files from application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -42,11 +42,11 @@
 //= require pleasure/globals/plugins/typehead.js/dist/typeahead.bundle.min
 //= require pleasure/globals/plugins/overlay/jquery.overlay
 //= require pleasure/globals/plugins/jquery-textcomplete/dist/jquery.textcomplete.min
-//= require pleasure/globals/plugins/emojify.js/emoji-list
+
 
 //= require pleasure/globals/plugins/ubilabs-geocomplete/jquery.geocomplete.min
 
-//= require pleasure/globals/plugins/pnikolov-bootstrap-colorpicker/dist/js/bootstrap-colorpicker.min
+
 //= require pleasure/globals/plugins/minicolors/jquery.minicolors.min
 //= require pleasure/globals/plugins/bootstrap-daterangepicker/daterangepicker
 
