a68e66864e813571765835cbefd9399023098ffb
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-05-06 08:13:22 -0400

    attendees/events confirmation ajax on events index

--- app/controllers/attendees_controller.rb
+++ app/controllers/attendees_controller.rb
@@ -2,6 +2,7 @@ class AttendeesController < ApplicationController
   
   def create
     @attendee = Attendee.create(user_id: params[:attendee][:user_id], event_id: params[:attendee][:event_id], yes: params[:attendee][:yes], maybe: params[:attendee][:maybe], no: params[:attendee][:no])
+    @user = User.friendly.find(params[:attendee][:user_id])
     @attendee.save
     @event = Event.find(@attendee.event_id)
     # @object = eval(@fan.fannable_type).find(@fan.fannable_id)
@@ -15,6 +16,7 @@ class AttendeesController < ApplicationController
   def update
     @attendee = Attendee.find(params[:id])
     @attendee.update_attributes(user_id: params[:attendee][:user_id], event_id: params[:attendee][:event_id], yes: params[:attendee][:yes], maybe: params[:attendee][:maybe], no: params[:attendee][:no])
+    @user = User.friendly.find(params[:attendee][:user_id])
     @event = Event.find(@attendee.event_id)
     respond_to do |format|
       format.js
