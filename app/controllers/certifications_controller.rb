15ba131d024ee196e382edf76eecf026b69e7263
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-05-06 13:56:25 -0400

    add certifications with ajax

--- app/controllers/certifications_controller.rb
+++ app/controllers/certifications_controller.rb
@@ -13,6 +13,7 @@ class CertificationsController < ApplicationController
         format.js
       end
     else
+      flash.now[:error] = @certification.errors.full_messages
       respond_to do |format|
         format.html{redirect_to :back}
         format.js
@@ -20,29 +21,41 @@ class CertificationsController < ApplicationController
     end
   end
   
-  def show
-    
-  end
-  
-  def edit
-    
-  end
-  
   def update
-    
+    @certification = Certification.find(params[:id])
+    @index = @certification.index_position
+    @before_item = @index - 1
+    @result = @certification.update_attributes(certification_params)
+    if @result
+      flash[:success] = "Certification has been updated."
+      respond_to do |format|
+        format.js
+        format.html { redirect_to :back }
+      end
+    else
+      flash.now[:error] = @certification.errors.full_messages
+      respond_to do |format|
+        format.js
+        format.html { redirect_to :back }
+      end
+    end
   end
   
   def destroy
-    
+    @certification = Certification.find(params[:id])
+    @index = @certification.index_position
+    @certification.destroy
+    respond_to do |format|
+      format.js
+      format.html { redirect_to :back }
+    end
   end
+  
   
   protected
   
   def certification_params
     params.require(:certification).permit(:name, :issuer)
   end
-  
-  def find_certification
-    
-  end
 end
+  
--- app/models/certification.rb
+++ app/models/certification.rb
@@ -1,2 +1,15 @@
 class Certification < ActiveRecord::Base
+  
+  
+  
+  def index_position
+    array = []
+    certifications = Certification.all
+    certifications.each do |c|
+      array << c.id
+    end
+    array.uniq.find_index(id) + 1
+  end
+  
+  
 end
