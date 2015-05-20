79d6f4e2ab84ff875183df483e9a44eab8ccdbc7
Khadijah Coakley <khadijah@wildfyregroup.com>
2015-05-20 14:23:47 -0400

    separate image uploaders based by ones that are croppable and ones that are not # all will be croppable eventually

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -1,25 +1,21 @@
 # encoding: utf-8
 
-class ImageUploader < CarrierWave::Uploader::Base
+class PhotoUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # storage :fog
-
   def extension_white_list
     %w(jpg jpeg gif png)
   end
   storage :fog
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
-  # def store_dir
-#     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-#   end
-
+  version :cropped do
+    process crop: :image
+  end
+  
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
