8eb58b3a30dff16d2a8729831edcc09bb014c21f
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-05-28 06:34:38 -0400

    setup logic/ajax

--- app/controllers/setup_controller.rb
+++ app/controllers/setup_controller.rb
@@ -24,52 +24,48 @@ class SetupController < ApplicationController
   def test_setup
     @athletes = User.athletes
   end
-
-  def setupsetup
-    @athletes = User.all
-  end
   
   
-  def athlete_setup
-    @team_ids = @user.relationships.where(participant: true).pluck(:team_id)
-    @adjusted_team_ids = @team_ids + params[:user][:team_ids].drop(1)
-    #create relationship
-    @adjusted_team_ids.each do |id|
-      relationship = Relationship.find_or_create_by(user_id: @user.id, team_id: id)
-      relationship.update_attributes(participant: true)
-    end
-    respond_to do |format|
-      format.js
-      format.html { redirect_to :back }
-    end
-  end
-  
-  def coach_setup 
-    @team_ids = @user.relationships.where(head: true).pluck(:team_id)
-    @adjusted_team_ids = @team_ids + params[:user][:team_ids].drop(1)
-    #create relationship
-    @adjusted_team_ids.each do |id|
-      relationship = Relationship.find_or_create_by(user_id: @user.id, team_id: id)
-      relationship.update_attributes(head: true)
-    end
-    respond_to do |format|
-      format.js
-      format.html { redirect_to :back }
-    end
-  end
-  
-  def athletic_director_setup
-    @school_id = params[:user][:athletic_director_ids]
-    if AthleticDirector.where(user_id: @user.id).present?
-      AthleticDirector.where(user_id: @user.id).last.destroy
-    end
-    AthleticDirector.create(user_id: @user.id, school_id: @school_id)
-    respond_to do |format|
-      format.js
-      format.html { redirect_to :back }
-    end
-  end
-  
+  # def athlete_setup
+#     @team_ids = @user.relationships.where(participant: true).pluck(:team_id)
+#     @adjusted_team_ids = @team_ids + params[:user][:team_ids].drop(1)
+#     #create relationship
+#     @adjusted_team_ids.each do |id|
+#       relationship = Relationship.find_or_create_by(user_id: @user.id, team_id: id)
+#       relationship.update_attributes(participant: true)
+#     end
+#     respond_to do |format|
+#       format.js
+#       format.html { redirect_to :back }
+#     end
+#   end
+#
+#   def coach_setup
+#     @team_ids = @user.relationships.where(head: true).pluck(:team_id)
+#     @adjusted_team_ids = @team_ids + params[:user][:team_ids].drop(1)
+#     #create relationship
+#     @adjusted_team_ids.each do |id|
+#       relationship = Relationship.find_or_create_by(user_id: @user.id, team_id: id)
+#       relationship.update_attributes(head: true)
+#     end
+#     respond_to do |format|
+#       format.js
+#       format.html { redirect_to :back }
+#     end
+#   end
+#
+#   def athletic_director_setup
+#     @school_id = params[:user][:athletic_director_ids]
+#     if AthleticDirector.where(user_id: @user.id).present?
+#       AthleticDirector.where(user_id: @user.id).last.destroy
+#     end
+#     AthleticDirector.create(user_id: @user.id, school_id: @school_id)
+#     respond_to do |format|
+#       format.js
+#       format.html { redirect_to :back }
+#     end
+#   end
+#
   
   protected
   
