5a15f2651614299cb90ea69cbc439a93662f0d8c
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-06-16 10:54:24 -0400

    add ability to sign in with phone number

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,20 +10,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.where(email: params[:email]).first
+    user = User.where(email: params[:identifier]).first if params[:identifier].include?("@")
+    user = User.where(mobile_phone_number: params[:identifier]).first unless params[:identifier].include?("@")
     if user && user.authenticate(params[:password])
       cookies.permanent[:authentication_token] = user.authentication_token
-      p "----------------"
       user.errors.full_messages
       redirect_to users_path
     else
       flash[:error] = "Email or password is incorrect."
-      render text: user.errors.full_messages
+      redirect_to :back
     end
-    # respond_to do |format|
-    #   format.json { render(json: flash.to_h); flash.clear }
-    #   format.html { redirect_to request.referrer }
-    # end
   end
 
   def show;end
