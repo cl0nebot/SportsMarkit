651221fed36883fb2feb382db0efbea39ab39157
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-06-16 14:36:27 -0400

    add ability to login with phone number

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,6 +18,7 @@ class SessionsController < ApplicationController
       redirect_to users_path
     else
       flash[:error] = "Email or password is incorrect."
+      p user.full_errors
       redirect_to :back
     end
   end
