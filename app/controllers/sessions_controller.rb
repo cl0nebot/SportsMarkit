cf4cecb78cf4ca8442e2b97558ec215ee36b6229
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-07-06 12:11:41 -0400

    add twilio notification for coach when athlete signs up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  require 'twilio-ruby'
 
 
   respond_to :html, :json
@@ -17,7 +18,14 @@ class SessionsController < ApplicationController
       if user.email.blank? and user.mobile_phone_number.present?
         redirect_to user_email_path(user)
       else
-        redirect_to users_path  
+        if @user.signin_count < 1 or @user.classifications.blank?
+          if user.mobile_phone_number.present? && Relationship.where(user_id: user.id, mobile_phone_number: user.mobile_phone_number)
+            send_mobile_notification(user)
+          end
+          redirect_to user_overview_path(user)
+        else
+          redirect_to users_path
+        end 
       end
     else
       flash[:error] = "Email or password is incorrect."
@@ -43,4 +51,29 @@ class SessionsController < ApplicationController
       format.json { render json: result }
     end
   end
+  
+  def send_mobile_notification(user)
+    relationship_ids = user.relationships.pluck(:team_id)
+    coach_user_ids = Relationship.where(team_id: relationship_ids, head: true, accepted: true).pluck(:user_id)
+    
+    User.where(user_id: coach_user_ids).each do |coach|
+      
+      receiving_number = coach.mobile_phone_number
+
+      twilio_sid = ENV['TWILIO_SID']
+      twilio_token = ENV['TWILIO_AUTH_TOKEN']
+      twilio_phone_number = "2025179077"
+
+      @twilio_client = Twilio::REST::Client.new twilio_sid, twilio_token
+
+      @twilio_client.account.sms.messages.create(
+        :from => "+1#{twilio_phone_number}",
+        :to => receiving_number,
+        :body => "Coach, #{user.first_name} has joined your SportsMarkit team!" )
+      end
+  end
+  
+  
+  
+  
 end
