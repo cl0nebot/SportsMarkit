18678abad5d199fa956f7aed90787fba11c349f6
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-07-06 13:57:00 -0400

    updated errors controller for fix timeout issue

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,7 +1,10 @@
 class ErrorsController < ApplicationController
   
   def not_found
-    render :status => 404, :formats => [:html]
+    respond_to do |format|
+      format.any(:htm, :html, :xls, :xlsx) { render :status => 404, :formats => [:html] }
+      format.all { render nothing: true, status: 404 }
+    end
   end
 
   def server_error
