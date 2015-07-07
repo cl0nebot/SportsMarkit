fabe30415f93ffb0939539f844eadb806ce2ef8f
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-07-07 17:58:51 -0400

    fixed homepage navigation

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -10,6 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery_ujs
 //= require pleasure/globals/js/global-vendors
 //= require pleasure/globals/plugins/scrollMonitor/scrollMonitor
 //=  pleasure/globals/plugins/skrollr/skrollr.min
