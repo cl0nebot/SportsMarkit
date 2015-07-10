9fce7ad6b02bcf3147ffca503d47b8d3b51e57df
Khadijah Coakley <khadijah@wildfyregroup.com>
2015-07-09 20:56:28 -0400

    small design updates overview page

--- db/migrate/20150709145242_create_league_sports.rb
+++ db/migrate/20150709145242_create_league_sports.rb
@@ -1,9 +1,9 @@
 class CreateLeagueSports < ActiveRecord::Migration
   def change
-    create_table :league_sports do |t|
-      t.integer :sport_id
-      t.integer :league_id
-      t.timestamps
+   create_table :league_sports do |t|
+     t.integer :sport_id
+    t.integer :league_id
+   t.timestamps
     end
-  end
+ end
 end
