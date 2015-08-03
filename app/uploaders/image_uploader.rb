1dbc2e91278f3cccf2c74d40f95bbf8757f47cb3
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-08-03 09:22:36 -0400

    photo uploaders - all photos have a custom id

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -9,10 +9,21 @@ class ImageUploader < CarrierWave::Uploader::Base
   # Choose what kind of storage to use for this uploader:
   # storage :fog
 
+  storage :fog
+  
   def extension_white_list
     %w(jpg jpeg gif png)
   end
-  storage :fog
+  
+  def filename
+      "#{secure_token}.#{file.extension}" if original_filename.present?
+    end
+
+    protected
+    def secure_token
+      var = :"@#{mounted_as}_secure_token"
+      model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+    end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -7,10 +7,21 @@ class PhotoUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
+  storage :fog
+  
   def extension_white_list
     %w(jpg jpeg gif png)
   end
-  storage :fog
+  
+  def filename
+      "#{secure_token}.#{file.extension}" if original_filename.present?
+    end
+
+    protected
+    def secure_token
+      var = :"@#{mounted_as}_secure_token"
+      model.instance_variable_get(var) or model.instance_variable_set(var, SecureRandom.uuid)
+    end
 
   version :cropped do
     process crop: :image
