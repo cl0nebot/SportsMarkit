0030297455b938d23a726c44d32483464b752861
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-08-19 09:57:18 -0400

    add access.rb concern for methods containing access #todo - make one method (?)

---
+++ app/models/concerns/access.rb
@@ -0,0 +1,44 @@
+module Access
+  extend ActiveSupport::Concern
+
+  def can_edit_team?(team)
+    if Relationship.where(user_id: id, head: true, team_id: team.id).present?
+      true
+    elsif AthleticDirector.where(user_id: id, school_id: team.school_id).present?
+      true
+    elsif admin?
+      true
+    else
+      false
+    end
+  end
+
+  def can_edit_school?(school)
+    if AthleticDirector.where(user_id: id, school_id: school.id).present?
+      true
+    elsif admin?
+      true
+    else
+      false
+    end
+  end
+
+  def can_edit_league?(league)
+    if LeagueManager.where(user_id: id, league_id: league.id).present?
+      true
+    elsif admin?
+      true
+    else
+      false
+    end
+  end
+  
+  def can_edit_facility?(facility)
+    
+  end
+  
+  def can_edit_user?(user)
+    
+  end
+
+end
