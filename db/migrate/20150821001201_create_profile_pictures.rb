8f01c05009446e475bb93d94a72b7bad300439d7
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-08-24 23:02:52 -0400

    remove contents from profile pictures migration

--- db/migrate/20150821001201_create_profile_pictures.rb
+++ db/migrate/20150821001201_create_profile_pictures.rb
@@ -1,9 +1,9 @@
 class CreateProfilePictures < ActiveRecord::Migration
-  def change
-    create_table :profile_pictures do |t|
-      t.references :profile_picture_owner, polymorphic: true, index: true
-      t.string :photo, :null => false
-      t.timestamps
-    end
-  end
+  # def change
+  #   create_table :profile_pictures do |t|
+  #     t.references :profile_picture_owner, polymorphic: true, index: true
+  #     t.string :photo, :null => false
+  #     t.timestamps
+  #   end
+  # end
 end
