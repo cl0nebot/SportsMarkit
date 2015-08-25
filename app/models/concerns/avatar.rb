4cf16a8873acc35d203a1d27fb671facd8250827
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-08-25 18:17:49 -0400

    added avatar to index views

--- app/models/concerns/avatar.rb
+++ app/models/concerns/avatar.rb
@@ -10,14 +10,33 @@ module Avatar
       end
     elsif self.class.to_s == "UserlessRelationship"
       "http://hasslefreeliving.com/wp-content/uploads/2012/10/placeholder.gif"
-    elsif ["School", "Facility", "Team", "League"].include? self.class.to_s
+    elsif self.class.to_s == "School"
       if profile_pictures.last.try(:id).blank?
-        "https://img-w.zeebox.com/images/z/f9b33d0f-d6f5-4676-98ea-e6254d7ad382.jpg"
+        "thumbs/school.jpg"
+      else
+        profile_pictures.last.photo
+      end
+    elsif self.class.to_s == "League"
+      if profile_pictures.last.try(:id).blank?
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Color_icon_gray_v2.svg/2000px-Color_icon_gray_v2.svg.png"
+      else
+        profile_pictures.last.photo
+      end
+    elsif self.class.to_s == "Team"
+      if profile_pictures.last.try(:id).blank?
+        "http://cache3.asset-cache.net/gc/187840132-soccer-team-celebrates-gettyimages.jpg?v=1&c=IWSAsset&k=2&d=ZQZzMJ8a0ONoFbdvawrO3ZhmEQ83NvPgJ9Rxm5hkoQw%3d"
+      else
+        profile_pictures.last.photo
+      end
+    elsif self.class.to_s == "Facility" 
+      if profile_pictures.last.try(:id).blank?
+        "placeholders/facility.png"
       else
         profile_pictures.last.photo
       end
     end
   end
+  
   
   
 end
