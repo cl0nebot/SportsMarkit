108b3222f09f718781735b93dba22ec6c1ebd828
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-09-02 15:38:10 -0400

    add can edit object method

--- app/models/concerns/access.rb
+++ app/models/concerns/access.rb
@@ -1,20 +1,10 @@
 module Access
   extend ActiveSupport::Concern
 
-  def can_edit_team?(team)
-    if Relationship.where(user_id: id, head: true, team_id: team.id).present?
+  def can_edit_team?(object)
+    if Relationship.where(user_id: id, head: true, team_id: object.id).present?
       true
-    elsif AthleticDirector.where(user_id: id, school_id: team.school_id).present?
-      true
-    elsif admin?
-      true
-    else
-      false
-    end
-  end
-
-  def can_edit_school?(school)
-    if AthleticDirector.where(user_id: id, school_id: school.id).present?
+    elsif AthleticDirector.where(user_id: id, school_id: object.school_id).present?
       true
     elsif admin?
       true
@@ -23,8 +13,18 @@ module Access
     end
   end
 
-  def can_edit_league?(league)
-    if LeagueManager.where(user_id: id, league_id: league.id).present?
+  def can_edit_school?(object)
+    if AthleticDirector.where(user_id: id, school_id: object.id).present?
+      true
+    elsif admin?
+      true
+    else
+      false
+    end
+  end
+
+  def can_edit_league?(object)
+    if LeagueManager.where(user_id: id, league_id: object.id).present?
       true
     elsif admin?
       true
@@ -33,12 +33,29 @@ module Access
     end
   end
   
-  def can_edit_facility?(facility)
+  def can_edit_facility?(object)
     
   end
   
-  def can_edit_user?(user)
-    id == user.id
+  def can_edit_user?(object)
+    id == object.id
+  end
+  
+  def can_edit_object?(object)
+    if admin?
+      true
+    elsif object.class.to_s == "Team"
+      can_edit_team?(object)
+    elsif object.class.to_s == "School"
+      can_edit_school?(object)
+    elsif object.class.to_s == "League"
+      can_edit_league?(object)
+    elsif object.class.to_s == "Facility"
+      can_edit_facility?(object)
+    elsif object.class.to_s == "User"
+      can_edit_user?(object)
+    end
+    
   end
 
 end
