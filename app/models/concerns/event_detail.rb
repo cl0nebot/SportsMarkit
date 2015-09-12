ce84ec704239cf7dce6f5b7e7ae3bdbe47dbbe99
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-09-12 14:08:32 -0400

    changed sort function to order to keep active record association instead of array

--- app/models/concerns/event_detail.rb
+++ app/models/concerns/event_detail.rb
@@ -11,19 +11,23 @@ module EventDetail
   
   def upcoming_events
     if self.class.to_s == "User"
-      all_events.where('ends_at >= ?', Time.now).sort_by(&:starts_at)
+      school_ids = user_schools.pluck(:id)
+      team_ids = user_teams.pluck(:id)
+      school_events = Event.where(eventable_type: "School", eventable_id: school_ids).where('ends_at >= ?', Time.now).order('starts_at ASC')
+      team_events = Event.where(eventable_type: "Team", eventable_id: team_ids).where('ends_at >= ?', Time.now).order('starts_at ASC')
+      events = (school_events + team_events).uniq
     elsif self.class.to_s == "School"
-      Event.where(eventable_type: "Team", eventable_id: team_ids).where('ends_at >= ?', Time.now).sort_by(&:starts_at)
+      Event.where(eventable_type: "Team", eventable_id: team_ids).where('ends_at >= ?', Time.now).order('starts_at ASC')
     elsif self.class.to_s == "League"
       team_ids = TeamLeague.where(league_id: id).pluck(:team_id)
       team_events_ids = Event.where(eventable_type: "Team", eventable_id: team_ids).where('ends_at >= ?', Time.now).pluck(:id)
       league_events_ids = Event.where(eventable_type: "League", eventable_id: id).where('ends_at >= ?', Time.now).pluck(:id)
-      events = Event.where(id: team_events_ids + league_events_ids ).uniq.sort_by(&:starts_at)
+      events = Event.where(id: team_events_ids + league_events_ids ).uniq.order('starts_at ASC')
     elsif self.class.to_s == "Team"
-      Event.where(eventable_type: "Team", eventable_id: id).where('ends_at >= ?', Time.now).sort_by(&:starts_at)
+      Event.where(eventable_type: "Team", eventable_id: id).where('ends_at >= ?', Time.now).order('starts_at ASC')
     elsif self.class.to_s == "Facility" 
       event_ids = EventFacility.where(facility_id: id).pluck(:event_id)
-      events = Event.where(id: event_ids ).where('ends_at >= ?', Time.now).sort_by(&:starts_at)
+      events = Event.where(id: event_ids ).where('ends_at >= ?', Time.now).order('starts_at ASC')
     end
   end
   
