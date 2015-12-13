b69cddb6fa46a8b2d5fde662f5d9169a414747ad
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-12-12 19:39:33 -0500

    update can_be_edited_by_user? method to take events

--- app/models/concerns/access.rb
+++ app/models/concerns/access.rb
@@ -1,67 +1,47 @@
 module Access
   extend ActiveSupport::Concern
-
-  def can_edit_team?(object)
-    if Relationship.where(user_id: id, head: true, team_id: object.id).present?
+  
+  
+  def can_be_edited_by_user?(current_user) #TODO make sure everything is covered: events, facility, user, team, school, league, club
+    if current_user.nil?
+      false
+    elsif current_user.admin?
       true
-    elsif AthleticDirector.where(user_id: id, school_id: object.school_id).present?
+    elsif self.class.to_s == "Event"
+      if user_id == current_user.id
+        true
+      else
+        object = eventable_type.constantize.find(eventable_id)
+        if object.can_be_edited_by_user?(current_user)
+          true
+        else
+          false
+        end
+      end
+      
+    elsif self.class.to_s == "User"
+      self.id == current_user.id ? true : false
+    elsif self.roles.where(user_id: current_user.id, status: "Active", role: ["Athletic Director", "Team Manager", "League Manager", "Coach", "Admin"]).present?
       true
-    elsif admin?
-      true
+    elsif 1==2
+      
+    elsif self.class.to_s == "Team"
+      if self.teamable.present? 
+        if Role.where(user_id: current_user.id, status: "Active", roleable_type: self.teamable.class.to_s, roleable_id: self.teamable.id, role: ["Athletic Director", "League Manager", "Admin"] ).present?
+          true
+        else
+          false
+        end
+      else
+        false
+      end
+   
     else
       false
     end
   end
-
-  def can_edit_school?(object)
-    if AthleticDirector.where(user_id: id, school_id: object.id).present?
-      true
-    elsif admin?
-      true
-    else
-      false
-    end
-  end
-
-  def can_edit_league?(object)
-    if LeagueManager.where(user_id: id, league_id: object.id).present?
-      true
-    elsif admin?
-      true
-    else
-      false
-    end
-  end
   
-  def can_edit_club?(object)
-    true
-  end
   
-  def can_edit_facility?(object)
-    
-  end
   
-  def can_edit_user?(object)
-    id == object.id
-  end
-  
-  def can_edit_object?(object)
-    if admin?
-      true
-    elsif object.class.to_s == "Team"
-      can_edit_team?(object)
-    elsif object.class.to_s == "School"
-      can_edit_school?(object)
-    elsif object.class.to_s == "League"
-      can_edit_league?(object)
-    elsif object.class.to_s == "Facility"
-      can_edit_facility?(object)
-    elsif object.class.to_s == "User"
-      can_edit_user?(object)
-    elsif object.class.to_s == "Club"
-       can_edit_club?(object)
-    end
-    
-  end
 
 end
