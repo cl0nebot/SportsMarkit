8f9e203f72e587eb12a88ac2269d9c874671cf84
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-12-14 23:12:55 -0500

    add extra catch for case where certificate does not contain certification_id

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -2,6 +2,12 @@ class Certificate < ActiveRecord::Base
   belongs_to :user
   belongs_to :certification
   validates_presence_of :user_id, :expiration, :certification_id
+  
+  after_create :delete_if_no_certification
+  
+  def delete_if_no_certification
+    self.delete if certification_id.nil?
+  end
 
   def fake_id
     @fake_id ||= id || SecureRandom.hex
