c135848495e558e99775ce5e1b2acf98ab942b66
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-12-14 23:35:23 -0500

    add video_loader.js to assets.rb preloaded files

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,4 +5,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( gmaps4rails.css main.css main.js)
+Rails.application.config.assets.precompile += %w( gmaps4rails.css main.css main.js video_loader.js)
