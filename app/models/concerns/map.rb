2e7a8cabcd2333292084bcad7c54d145c0e4a459
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-12-14 23:07:35 -0500

    remove  geo.rb concern - renamed to map.rb

--- app/models/concerns/map.rb
+++ app/models/concerns/map.rb
@@ -1,9 +1,9 @@
-module Geo
+module Map
   extend ActiveSupport::Concern
   
   def gmaps4rails_address
   #describe how to retrieve the address from your model, if you use directly a db column, you can dry your code, see wiki
-    "#{self.address_1}, #{self.city}, #{self.state}" 
+    "#{self.address.street_1}, #{self.address.city}, #{self.address.state}" 
   end
 
   def city_state
