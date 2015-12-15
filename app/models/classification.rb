10f35df7ad2f8ccf978e780f6f9b1bbe8bfb59fe
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-12-14 23:12:16 -0500

    add custom validation to classification.rb

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -1,3 +1,11 @@
 class Classification < ActiveRecord::Base
   belongs_to :user
+  validate :not_duplicate
+  
+  def not_duplicate
+    existing_classification = Classification.where(user_id: self.user_id, classification: self.classification)
+    if existing_classification.present?
+      errors.add(:user_id, "#{self.classification} classification already exists for this user.")
+    end
+  end
 end
