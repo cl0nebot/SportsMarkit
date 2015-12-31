1521eff3a91a769dae379ef3a5f26d18c0129b71
Cordis Stanfield <cordis.stanfield@gmail.com>
2015-12-31 04:14:45 -0500

    fixed all_events and upcoming_events methods

--- app/models/concerns/event_detail.rb
+++ app/models/concerns/event_detail.rb
@@ -1,42 +1,47 @@
 module EventDetail
   extend ActiveSupport::Concern
-  
-  def all_events
-    if self.class.to_s == "Team"
-      events
-    else
-      upcoming_events
-    end
-  end
-  
+    
   def next_event
     upcoming_events.first.nil? ? "No upcoming events" : upcoming_events.first.title
   end
   
-  def upcoming_events
+  def all_events
     if self.class.to_s == "User"
       school_ids = user_schools.pluck(:id)
       team_ids = user_teams.pluck(:id)
-      school_events = Event.where(eventable_type: "School", eventable_id: school_ids).where('ends_at >= ?', Time.now).order('starts_at ASC')
-      team_events = Event.where(eventable_type: "Team", eventable_id: team_ids).where('ends_at >= ?', Time.now).order('starts_at ASC')
-      user_events = self.events.where('ends_at >= ?', Time.now).order('starts_at ASC')
-      events = (school_events + team_events + user_events).uniq
+      school_events = Event.where(eventable_type: "School", eventable_id: school_ids).pluck(:id)
+      team_events = Event.where(eventable_type: "Team", eventable_id: team_ids).pluck(:id)
+      opponent_team_events = Event.where(opponent_type: "Team", opponent_id: team_ids).pluck(:id)
+      user_events = self.events.pluck(:id)
+      events = Event.where(id: (school_events + team_events + user_events + opponent_team_events).uniq)
     elsif self.class.to_s == "School"
-      Event.where(eventable_type: "Team", eventable_id: team_ids).where('ends_at >= ?', Time.now).order('starts_at ASC')
+      school = Event.where(eventable_type: "School", eventable_id: id).pluck(:id)
+      school_team = Event.where(eventable_type: "Team", eventable_id: team_ids).pluck(:id)
+      opponent_school_team = Event.where(opponent_type: "Team", opponent_id: team_ids).pluck(:id)
+      Event.where(id: (school + school_team + opponent_school_team).uniq)
     elsif self.class.to_s == "League"
-      team_ids = TeamLeague.where(league_id: id).pluck(:team_id)
+      team_ids = Connect.where(ownerable_type: "Team", connectable_id: id, connectable_type: "League").pluck(:ownerable_id)
       team_events_ids = Event.where(eventable_type: "Team", eventable_id: team_ids).pluck(:id)
+      opponent_team_events_ids = Event.where(opponent_type: "Team", opponent_id: team_ids).pluck(:id)
       league_events_ids = Event.where(eventable_type: "League", eventable_id: id).pluck(:id)
-      events = Event.where(id: (team_events_ids + league_events_ids).uniq ).order('starts_at ASC')
+      events = Event.where(id: (team_events_ids + league_events_ids + opponent_team_events_ids).uniq )
     elsif self.class.to_s == "Team"
-      Event.where(eventable_type: "Team", eventable_id: id).where('ends_at >= ?', Time.now).order('starts_at ASC')
+      team = Event.where(eventable_type: "Team", eventable_id: id).pluck(:id)
+      opponent = Event.where(opponent_type: "Team", opponent_id: id).pluck(:id)
+      Event.where(id: (team + opponent).uniq)
     elsif self.class.to_s == "Facility" 
-      event_ids = EventFacility.where(facility_id: id).pluck(:event_id)
-      events = Event.where(id: event_ids ).where('ends_at >= ?', Time.now).order('starts_at ASC')
-    elsif self.class.to_s == "Club" 
-      event_ids = EventFacility.where(facility_id: id).pluck(:event_id)
-      events = Event.where(id: event_ids ).where('ends_at >= ?', Time.now).order('starts_at ASC')
+      event_ids = Connect.where(eventable_type: "Event", connectable_id: id, connectable_type: "Facility").pluck(:eventable_id)
+      events = Event.where(id: event_ids )
+    elsif self.class.to_s == "Club"
+      club = Event.where(eventable_type: "Club", eventable_id: id).pluck(:id)
+      club_team = Event.where(eventable_type: "Team", eventable_id: team_ids).pluck(:id)
+      opponent_club_team = Event.where(opponent_type: "Team", opponent_id: team_ids).pluck(:id)
+      Event.where(id: (club_team + opponent_club_team + club).uniq )
     end
+  end
+  
+  def upcoming_events
+    all_events.where('ends_at >= ?', Time.now).order('starts_at ASC')
   end
   
  
