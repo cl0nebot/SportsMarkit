41bb0e61425a56d4787e13273fb7d72d473b391e
Cordis Stanfield <cordis.stanfield@gmail.com>
2016-03-03 16:55:16 -0500

    add dmitri to exception notifier

--- config/initializers/exception_notifier.rb
+++ config/initializers/exception_notifier.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use ExceptionNotification::Rack,
   :email => {
-    :email_prefix => "[PREFIX] ",
+    :email_prefix => "[SPORTSMARKIT ERROR] ",
     :sender_address => %{"notifier" <notifier@example.com>},
-    :exception_recipients => %w{accounts@wildfyregroup.com}
+    :exception_recipients => %w{accounts@wildfyregroup.com dmitri@wildfyregroup.com}
   }
