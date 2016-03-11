d9feb0f78ca717aa1ee72d58492e5889123b01b0
Dmitri Kiselevich <kis.dmitri.gen@gmail.com>
2016-03-11 22:25:44 +0200

    fixed login form validation

--- app/assets/javascripts/validation/user_login_validation.js
+++ app/assets/javascripts/validation/user_login_validation.js
@@ -1,7 +1,7 @@
 $(function(){
   if($('div').is('#pane-login')){
     login_validation();
-    $( "input[value='Login']" ).attr('disabled', true);
+    $("input[value='Login']").attr('disabled', true);
   }
 })
 
@@ -13,9 +13,8 @@ function login_validation() {
 
 function init_user_password_validation() {
   $('#password').on("keyup change click", function() {
-    var object = $(this)
     var objectVal = $(this).val();
-     if (objectVal == "" ) {
+    if (objectVal == "") {
       $(this).parent().siblings('.password_field').html("<font size='2' color='red'>This field is required.</font>")
     } else if (objectVal.length < 6) {
       $(this).parent().siblings('.password_field').html("<font size='2' color='red'>Must be at least 6 characters.</font>")
@@ -30,17 +29,13 @@ function init_user_identifier_validation() {
     var object = $(this)
     var objectVal = $(this).val();
     var pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i
-    $.get('/checkemail?email='+objectVal,function(data){
-      if (objectVal == "" ) {
-        object.parent().siblings('.identifier_field').html("<font size='2' color='red'>This field is required.</font>")
-      } else if (!(pattern.test(objectVal))) {
-        object.parent().siblings('.identifier_field').html("<font size='2' color='red'>" + objectVal +  " is not valid.</font>")
-      } else if (data.email_exists == false) {
-        object.parent().siblings('.identifier_field').html("<font size='2' color='red'>" + objectVal +  " is not present.</font>")
-      } else {
-        object.parent().siblings('.identifier_field').html("<font size='2' color='green'>Looks good.<b>✓</b> </font>")
-      }
-    })
+    if (objectVal == "") {
+      object.parent().siblings('.identifier_field').html("<font size='2' color='red'>This field is required.</font>")
+    } else if (!(pattern.test(objectVal) || objectVal.match(/^\d{10}$/))) {
+      object.parent().siblings('.identifier_field').html("<font size='2' color='red'>" + objectVal +  " is not valid.</font>")
+    } else {
+      object.parent().siblings('.identifier_field').html("<font size='2' color='green'>Looks good.<b>✓</b> </font>")
+    }
   })
 }
 
@@ -49,20 +44,16 @@ function init_login_form_validation() {
     var userPassword = $(this).find('#password').val();
     var userIdentifier = $(this).find('#identifier').val();
     var pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i
-    $.get('/checkemail?email='+userIdentifier,function(data){
-      if (userPassword == "" ) {
-        $( "input[value='Login']" ).attr('disabled', true);
-      } else if (userPassword.length < 6) {
-        $( "input[value='Login']" ).attr('disabled', true);
-      } else if (userIdentifier == "") {
-        $( "input[value='Login']" ).attr('disabled', true);
-      } else if (!(pattern.test(userIdentifier))) {
-        $( "input[value='Login']" ).attr('disabled', true);
-      } else if (data.email_exists == false) {
-        $( "input[value='Login']" ).attr('disabled', true);
-      } else {
-        $( "input[value='Login']" ).attr('disabled', false);
-      }
-    })
+    if (userPassword == "") {
+      $("input[value='Login']").attr('disabled', true);
+    } else if (userPassword.length < 6) {
+      $("input[value='Login']").attr('disabled', true);
+    } else if (userIdentifier == "") {
+      $("input[value='Login']").attr('disabled', true);
+    } else if (!(pattern.test(userIdentifier) || userIdentifier.match(/^\d{10}$/))) {
+      $("input[value='Login']").attr('disabled', true);
+    } else {
+      $("input[value='Login']").attr('disabled', false);
+    }
   })
 }
