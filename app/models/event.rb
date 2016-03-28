aa948c1dec7fd8b2232de76198dc5e857015d140
Dmitri Kiselevich <kis.dmitri.gen@gmail.com>
2016-03-28 15:18:33 +0300

    added validation to teams

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,5 @@
 class Event < ActiveRecord::Base
   include ActiveModel::Serialization
-
   include Access
   extend FriendlyId
 
@@ -28,11 +27,11 @@ class Event < ActiveRecord::Base
   def self.between(start_time, end_time)
     where('starts_at >= ?', Event.format_date(start_time)).where('starts_at <= ?', Event.format_date(end_time)).where.not('starts_at > ?', Event.format_date(end_time)).uniq
   end
-  
+
   def self.upcoming_events
     where('ends_at >= ?', Time.now).order('starts_at ASC')
   end
-  
+
   def use_for_slug
     existing_event = Event.where('slug = ?', self.slug)
     if existing_event.present?
@@ -44,21 +43,20 @@ class Event < ActiveRecord::Base
 
   def as_json(options = {})
     {
-        :id => self.id,
-        :title => self.title,
-        :description => self.description || "",
-        :start => starts_at.localtime.strftime('%Y-%m-%d %H:%M'),
-        :end => ends_at.localtime.strftime('%Y-%m-%d %H:%M'),
-        :allDay => self.all_day,
-        :recurring => false,
-        #:url => Rails.application.routes.url_helpers.event_path(),
-        :url => "/events/#{self.slug}",
-        :color => self.color
+      id: self.id,
+      title: self.title,
+      description: self.description || "",
+      start: starts_at.localtime.strftime('%Y-%m-%d %H:%M'),
+      end: ends_at.localtime.strftime('%Y-%m-%d %H:%M'),
+      allDay: self.all_day,
+      recurring: false,
+      url: "/events/#{self.slug}",
+      color: self.color
     }
   end
-  
+
   def color
-    if ["Game", "Track Meet"].include? event_type 
+    if ["Game", "Track Meet"].include? event_type
       "red"
     elsif ["Practice"].include? event_type
       "blue"
@@ -86,23 +84,23 @@ class Event < ActiveRecord::Base
     self.starts_at = Time.strptime(starts.strip, '%m/%d/%Y %H:%M %P')
     self.ends_at = Time.strptime(ends.strip, '%m/%d/%Y %H:%M %P')
   end
-  
+
   def people_that_are_going
     attendees.where(yes: true)
   end
-  
+
   def name_and_phone_numbers
     array = []
     attendees.each do |att|
-      array << [att.user.first_name, att.user.mobile_phone_number] 
+      array << [att.user.first_name, att.user.mobile_phone_number]
     end
     array
   end
-  
+
   def facility
     Facility.find(connects.where(connectable_type: "Facility").last.connectable_id)
   end
-  
+
   def proper_facility_name
     if facility.school.present?
       "#{facility.school.name} #{facility.name}"
@@ -110,18 +108,16 @@ class Event < ActiveRecord::Base
       "#{facility.name}"
     end
   end
-  
+
   def proper_facility_name_and_location
     if facility.school.present?
       "#{facility.school.name} #{facility.name}, #{facility.address.city_and_state}"
     else
       "#{facility.name} #{facility.address.city_and_state}"
     end
-    
   end
 
   def has_similar?
     !single? || parent_id
   end
-
 end
