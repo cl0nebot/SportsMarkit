65b07aac20e39f38da55098f7a995704b1b0802c
Dmitri Kiselevich <kis.dmitri.gen@gmail.com>
2016-03-30 13:34:41 +0300

    show excel errors on uploads form

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,10 +1,10 @@
 class UploadsController < ApplicationController
   before_action :find_object, only: [:index]
-  
+
   def index
-    
+
   end
-  
+
   def create
     model = params[:object].constantize
     importer = "Importer::#{params[:importer]}".constantize
@@ -16,10 +16,8 @@ class UploadsController < ApplicationController
     end
 
     if @import.failure?
-      if @import.error_xls
-        send_data(@import.error_xls.read, type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', filename: "import-error-#{Time.now}.xlsx")
-        return
-      end
+      binding.pry
+      redirect_to :back, alert: @import.errors
     else
       redirect_to :back, notice: 'Uploaded'
     end
@@ -30,39 +28,3 @@ class UploadsController < ApplicationController
     @object = $1.capitalize.constantize.find(params[param])
   end
 end
-
-
-# class UploadsController < ApplicationController
-#   before_action :find_object, only: [:index]
-#
-#   def index
-#
-#   end
-#
-#   def import
-#     model = params[:object].constantize
-#     if params[:school_id].present?
-#       model.import_team(params[:file], params[:school_id])
-#     else
-#       model.import(params[:file], model)
-#     end
-#     redirect_to :back, notice: "Uploaded."
-#   end
-#
-#   def roster
-#     model = params[:object].constantize
-#     if params[:team_id].present?
-#       model.import_team(params[:file], params[:team_id])
-#     else
-#     end
-#     redirect_to :back, notice: "Uploaded."
-#   end
-#
-#   private
-#
-#   def find_object
-#     param = params.keys.find{|key| key =~ /(\w+)_id/}
-#     @object = $1.capitalize.constantize.find(params[param])
-#   end
-#
-# end
