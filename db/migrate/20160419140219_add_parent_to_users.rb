95b2a24070e513c8552c9b3e0f0ad88c271b6a38
Cordis Stanfield <cordis.stanfield@gmail.com>
2016-04-26 09:22:42 -0400

    changed add parent to users migraton

--- db/migrate/20160419140219_add_parent_to_users.rb
+++ db/migrate/20160419140219_add_parent_to_users.rb
@@ -1,6 +1,6 @@
 class AddParentToUsers < ActiveRecord::Migration
   def change
-    add_reference :users, :parent, references: :users, index: true
+    add_reference :users, :parent, index: true
     change_column_null :users, :email, true
     change_column_default :users, :email, nil
     add_index :users, :email, :unique => true
