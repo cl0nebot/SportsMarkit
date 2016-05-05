b18a0e0e5e20f8e09931d58bb2a0bc7221794955
Dmitri Kiselevich <kis.dmitri.gen@gmail.com>
2016-05-05 20:59:31 +0300

    don"t need user birthday form fb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,7 @@ require "omniauth-facebook"
 require "omniauth-google-oauth2"
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"],{:scope => "user_birthday",:image_size => 'large', secure_image_url: true}
+  provider :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"],{:image_size => 'large', secure_image_url: true}
   provider :google_oauth2, ENV["GOOGLE_APP_ID"], ENV["GOOGLE_SECRET"], { access_type: "offline", approval_prompt: ""}
   provider :twitter, ENV["TWITTER_APP_ID"], ENV["TWITTER_SECRET"] 
 end
