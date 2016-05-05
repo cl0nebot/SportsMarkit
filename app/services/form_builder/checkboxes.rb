dde7b5f6fb21d72f048db309b811f6ea23823eb3
Dmitri Kiselevich <kis.dmitri.gen@gmail.com>
2016-05-05 16:51:57 +0300

    fix es for checkboxes and required names

--- app/services/form_builder/checkboxes.rb
+++ app/services/form_builder/checkboxes.rb
@@ -1,15 +1,16 @@
 class FormBuilder::Checkboxes < FormBuilder::Base
   def field_tag
     field_options.each_with_index.map do |checkbox, index|
-      tag = check_box_tag(field_name, checkbox["label"], checkbox["checked"] == "checked", base_options(index: index))
-      tag + label_tag("#{field_name}_#{index}", "#{checkbox['label']}")
+      rand = SecureRandom.hex
+      tag = check_box_tag(field_name, checkbox["label"], checkbox["checked"] == "checked", base_options(index: index, rand: rand))
+      tag + label_tag("#{field_name}_#{index}_#{rand}", "#{checkbox['label']}")
     end.join
   end
 
   def base_options(opt)
     options = {
       class: "#{field_name} #{ 'required' if required?}",
-      id: "#{field_name}_#{opt[:index]}",
+      id: "#{field_name}_#{opt[:index]}_#{opt[:rand]}",
       name: "user_data[#{field_name}][]"
     }
     options.merge!({ "data-required-any" => ".#{field_name}" }) if required?
