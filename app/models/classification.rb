e6dc2e75db2fc821dac03d5bcbaa4d3e2737646a
Cordis Stanfield <cordis.stanfield@gmail.com>
2016-05-19 07:36:19 -0400

    hooked up classification snippet for onboarding

--- app/models/classification.rb
+++ app/models/classification.rb
@@ -2,6 +2,27 @@ class Classification < ActiveRecord::Base
   belongs_to :user
   validate :not_duplicate, on: :create
   
+  def self.description(classification)
+    case classification
+    when "Student Athlete"
+      "I am a student."
+    when "Athlete"
+      "I play on recreational teams"
+    when "Coach"
+      "I coach athletes"
+    when "Guardian"
+      "I am a parent of an athlete"
+    when "Athletic Director"
+      "I manage schools"
+    when "Club Director"
+      "I manage clubs"
+    when "School Manager"
+      "I manage schools"
+    when "Team Manager"
+      "I manage teams"
+    end
+  end
+  
   def not_duplicate
     existing_classification = Classification.where(user_id: self.user_id, classification: self.classification)
     if existing_classification.present?
